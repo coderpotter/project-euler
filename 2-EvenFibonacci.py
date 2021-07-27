888f83dc8a5dbe9584972cba3cf119609c174b95
coderpotter <anighojkar@usf.edu>
2021-07-27 12:31:59 -0400

    corrected 2

--- 2-EvenFibonacci.py
+++ 2-EvenFibonacci.py
@@ -1,14 +1,13 @@
 """
 By considering the terms in the Fibonacci sequence whose values do not exceed N, find the sum of the even-valued terms. Assume that N will always be greater than 1.
 """
-from tqdm import tqdm
 
 N = int(input("Enter N: "))
 a, b, c = 1, 2, 3
-ans = b
-for i in tqdm(range(1, (N + 1) // 3)):
+ans = 0
+while b < N:
+    ans += b
     a = b + c
     b = a + c
     c = a + b
-    ans += b
 print(ans)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # project-euler
 All the questions solved in Project Euler (https://projecteuler.net/)
+
+For progress bars, run ```pip install tqdm```.
