9bc0f5475d61c04f65e9e05ef870965f72aa5fa4
coderpotter <anighojkar@usf.edu>
2021-10-19 00:25:41 -0400

    4

---
+++ 4-LargestPalindromeProduct.py
@@ -0,0 +1,26 @@
+"""
+Find the largest palindrome made from the product of two N-digit numbers.
+"""
+
+import utils
+from itertools import product
+
+N = int(input("Enter N: "))
+
+if N%2:
+    a, maxProd = 10**N - 1, [0, 0]
+    while (a > maxProd[1]):
+        b = a
+        while (b > 10**(N-1)):
+            ab = str(a*b)
+            if utils.is_palindrome(ab):
+                if utils.list_product([a, b]) > utils.list_product(maxProd):
+                    maxProd = [a, b]
+                break
+            else:
+                b -= 1
+        a -= 1
+    print(maxProd[0], maxProd[1], utils.list_product(maxProd))
+else:
+    print(10**N - 1, '9'*(N//2)+'0'*(N//2 - 1) + '1')
+
--- utils.py
+++ utils.py
@@ -1,2 +1,10 @@
+import math
+
 def swap(a, b):
     return b, a
+
+def is_palindrome(a): # takes 2 strings
+    return a == a[::-1]
+
+def list_product(L): # returns the product of a list
+    return math.prod(L)
